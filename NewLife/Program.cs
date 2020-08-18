71d7debcc2f602c63ab2267cb8be8335ccc65b8d
TaigaDesuu <69847374+TaigaDesuu@users.noreply.github.com>
2020-08-18 14:07:55 +0300

    Тут я исправил

---
+++ NewLife/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wo!");
+
+        }
+    }
+}
